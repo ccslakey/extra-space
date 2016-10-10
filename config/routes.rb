668cafe5b4268da58699f24d68908e43c6f70ded
ccslakey <cromwellslakey@gmail.com>
2016-10-09 18:54:46 -0700

    first commit with devise_scope

---
+++ config/routes.rb
@@ -0,0 +1,21 @@
+Rails.application.routes.draw do
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # root 'pages#landing_page'
+  devise_for :users, controllers: {
+    confirmations: 'users/confirmations',
+    passwords: 'users/passwords',
+    registrations: 'users/registrations',
+    sessions: 'users/sessions',
+    unlocks: 'users/unlocks',
+  }
+
+  devise_scope :user do
+    authenticated :user do
+      root to: 'pages#dashboard', as: :authenticated_root
+    end
+    unauthenticated :user do
+      root to: 'users/sessions#new'
+    end
+    patch '/users/confirmation' => 'users/confirmations#update'
+  end
+end
